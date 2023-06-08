56aea2f36c14a286406af53239dfa7b05e43b370
kdvajay <79804499+kdvajay@users.noreply.github.com>
2023-06-09 00:14:53 +0530

    Completed the assignment

---
+++ container.js
@@ -0,0 +1,34 @@
+var html;
+window.onload = function(){
+	html = document.getElementById('DnD').innerHTML;
+};
+var container = document.getElementById("DnD");
+var content = container.innerHTML;
+
+ function allowDrop(ev) {
+   ev.preventDefault();
+ }
+
+ function drag(ev) {
+   ev.dataTransfer.setData("text", ev.target.id);
+ }
+
+ function drop(ev) {
+   ev.preventDefault();
+   var data = ev.dataTransfer.getData("text");
+   var evtarget = ev.target;
+   if (evtarget == "[object HTMLImageElement]") {
+     evtarget = evtarget.parentNode;
+   } else {
+     ev.target.appendChild(document.getElementById(data));
+     alert(JSON.stringify("Item dropped successfully!"));
+   }
+
+ }
+
+
+
+ function reset() {
+   var container = document.getElementById("DnD");
+   container.innerHTML = content;
+ }
